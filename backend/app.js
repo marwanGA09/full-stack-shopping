e25550241240087295dc2bbea7fcd3855d794cb0
Adem Kedir <ademkedir724@gmail.com>
2024-07-17 12:24:37 +0300

    add products route

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const fs = require('fs');
 const morgan = require('morgan');
 
+const myJSON = JSON.parse(
+  fs.readFileSync(`${__dirname}/json/data.json`, 'utf-8')
+).products;
+console.log(myJSON);
 const app = express();
 
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('<p>Here is express</p>');
+});
+
+app.get('/products', (req, res) => {
+  console.log(myJSON);
+  res.json(myJSON);
 });
 
 const PORT = process.env.PORT || 8888;
