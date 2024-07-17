1d244f3d2748e3bda9ad59cb3c061b25f59175aa
Adem Kedir <ademkedir724@gmail.com>
2024-07-17 12:45:56 +0300

    add product/:id route

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ const morgan = require('morgan');
 const myJSON = JSON.parse(
   fs.readFileSync(`${__dirname}/json/data.json`, 'utf-8')
 ).products;
-console.log(myJSON);
+// console.log(myJSON);
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -17,6 +18,13 @@ app.get('/', (req, res) => {
 app.get('/products', (req, res) => {
   console.log(myJSON);
   res.json(myJSON);
+});
+
+app.get('/products/:id', (req, res) => {
+  const id = req.params.id;
+  const currentProduct = myJSON.find((product) => product.id === id);
+  if (currentProduct) res.json(currentProduct);
+  res.status(404).end();
 });
 
 const PORT = process.env.PORT || 8888;
