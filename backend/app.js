8079989dba92174c77a5fdf652e083ad5dcfd13d
Adem Kedir <ademkedir724@gmail.com>
2024-07-17 12:56:28 +0300

    use cors library

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
+// ALLOW TO BE ACCESSED FROM SPECIFIED OR ANYWHERE ON INTERNAT
+const cors = require('cors');
 
 const myJSON = JSON.parse(
   fs.readFileSync(`${__dirname}/json/data.json`, 'utf-8')
@@ -8,6 +10,15 @@ const myJSON = JSON.parse(
 // console.log(myJSON);
 
 const app = express();
+
+// from any where, ONLY DEV MODE
+// app.use(cors());
+
+// PRODUCTION MODE
+const corsOption = {
+  origin: 'http://localhost:5173',
+};
+app.use(cors(corsOption));
 
 app.use(morgan('dev'));
 
--- frontend/src/fetcher.jsx
+++ frontend/src/fetcher.jsx
@@ -2,12 +2,23 @@ import axios from 'axios';
 
 export default async function fetcher(params) {
   const url = params
-    ? `http://localhost:3001/products/${params}`
-    : `http://localhost:3001/products`;
+    ? `http://localhost:8888/products/${params}`
+    : `http://localhost:8888/products`;
+
+  // JSON SERVER FILE
+  // const url = params
+  //   ? `http://localhost:3001/products/${params}`
+  //   : `http://localhost:3001/products`;
   try {
     const res = await axios.get(url);
     if (res.statusText.toLowerCase() !== 'ok') {
-      throw new Error('Something went Wrong, while fetching');
+      // Check for CORS error based on specific status codes or response format
+      if (res.status === 403) {
+        // This is a suggestion, check server specific behavior
+        throw new Error('CORS Error: Access Denied');
+      } else {
+        throw new Error('Something went Wrong, while fetching');
+      }
     }
     return res.data;
   } catch (err) {
