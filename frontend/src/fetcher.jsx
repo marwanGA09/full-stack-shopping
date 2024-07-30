e909dc2a44779e5d3434768eebadde27b28d788c
Adem Kedir <ademkedir724@gmail.com>
2024-07-30 11:49:44 +0300

    change how to handle error for fetcher component

--- frontend/src/fetcher.jsx
+++ frontend/src/fetcher.jsx
@@ -6,15 +6,7 @@ export default async function fetcher(params) {
   try {
     const res = await axios.get(url);
     console.log('res', res);
-    if (res.statusText.toLowerCase() !== 'ok') {
-      // Check for CORS error based on specific status codes or response format
-      if (res.status === 403) {
-        // This is a suggestion, check server specific behavior
-        throw new Error('CORS Error: Access Denied');
-      } else {
-        throw new Error('Something went Wrong, while fetching');
-      }
-    }
+
     return res.data;
   } catch (err) {
     console.log('error', err);
