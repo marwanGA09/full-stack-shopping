bf94a881b04af15bf727546fb1321c24d039d62f
Adem Kedir <ademkedir724@gmail.com>
2024-07-31 10:36:07 +0300

    work currently to local system

--- frontend/src/URL/url.js
+++ frontend/src/URL/url.js
@@ -1,4 +1,4 @@
-const myURL = `https://m-shopping-cart-api.onrender.com`;
-// const myURL = `http://localhost:8888`;
+// const myURL = `https://m-shopping-cart-api.onrender.com`;
+const myURL = `http://localhost:8888`;
 
 export default myURL;
