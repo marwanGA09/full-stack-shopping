70d96846b5bf7b0eded5c06d04f7dfc2197aaad5
Adem Kedir <ademkedir724@gmail.com>
2024-08-02 14:06:02 +0300

    add global error catch route and handle using middleware

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const productRoute = require('./router/products.js');
+const AppError = require('./utils/appError.js');
 
 const app = express();
 // from any where, ONLY DEV MODE
@@ -21,5 +22,21 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/v1/products', productRoute);
+
+// GLOBAL ERROR CONTROL
+app.all('*', (req, res, next) => {
+  next(new AppError(`${req.originalUrl} this address is not known`, 500));
+  //   next(new Error(`${req.originalUrl} this address is not known`));
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || 'server internal error';
+
+  res.status(err.statusCode).json({
+    status: err.status || 'error',
+    message: err.message,
+  });
+});
 
 module.exports = app;
