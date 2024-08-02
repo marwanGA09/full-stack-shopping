f0fcd44e5368d652ea1ff7463975aee199fa4b1e
Adem Kedir <ademkedir724@gmail.com>
2024-08-02 20:33:40 +0300

    add errorStack to error class

--- backend/utils/appError.js
+++ backend/utils/appError.js
@@ -4,7 +4,20 @@ class AppError extends Error {
     this.statusCode = statusCode;
     this.status = statusCode * 1 >= 500 ? 'error' : 'failure';
     this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
 module.exports = AppError;
+
+/*
+  Error.captureStackTrace(this, this.constructor);
+Purpose
+The line Error.captureStackTrace(this, this.constructor); within the AppError constructor is essential for providing a more informative stack trace when an error of this type is thrown.
+
+Breakdown
+Error.captureStackTrace: This is a static method on the Error object in Node.js. It's used to capture a stack trace for a given object.
+this: Refers to the current instance of the AppError class being constructed.
+this.constructor: Refers to the constructor function of the AppError class.
+
+*/
