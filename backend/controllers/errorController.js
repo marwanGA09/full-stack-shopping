9dc73d28e6ff9a1a0160c172dcb1b30df1953ad9
Adem Kedir <ademkedir724@gmail.com>
2024-08-02 23:16:37 +0300

    refactor error controller

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -1,20 +1,41 @@
-module.exports = (err, req, res, next) => {
-  //   console.log(err.stack);
+const devErrorSend = (err, res) => {
+  console.log('dev error');
+  return res.status(err.statusCode).json({
+    status: err.status || 'error',
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+const prodErrorSend = (err, res) => {
+  // BELIEVABLE ERROR
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status || 'error',
+      message: err.message,
+    });
+  } else {
+    // Unknown error
+    console.error('ERROR 🔥', err);
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: 'something went wrong',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  console.log('here error');
+  //   console.log(err.stack);
+  console.log('process', process.env);
+  console.log('process node', process.env.NODE_ENV);
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'server internal error';
 
   if (process.env.NODE_ENV === 'production') {
-    res.status(err.statusCode).json({
-      status: err.status || 'error',
-      message: err.message,
-    });
+    prodErrorSend(err, res);
   } else if (process.env.NODE_ENV === 'development') {
-    res.status(err.statusCode).json({
-      status: err.status || 'error',
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
+    devErrorSend(err, res);
   }
 };
