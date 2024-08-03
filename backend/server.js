4fde9accd5196d58d131ec393d21abac5cdce920
Adem Kedir <ademkedir724@gmail.com>
2024-08-03 10:03:26 +0300

    add global unhandledRejection handler and uncaughtException handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
-const app = require('./app');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION');
+  console.log(err);
+  process.exit(1);
+});
+
+const app = require('./app');
 
 dotenv.config({ path: './.env' });
 
@@ -15,4 +22,12 @@ mongoose.connect(DB_URL).then((conn) => {
 
 const PORT = process.env.PORT || 8888;
 
-app.listen(PORT, () => console.log('server listening on ', PORT));
+const server = app.listen(PORT, () =>
+  console.log('server listening on ', PORT)
+);
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION');
+  console.log(err);
+  server.close(() => process.exit(1));
+});
